cf2057718ded3a526a441eb0545157720c2d795e
mag123c <diehreo@gmail.com>
2025-04-22 21:39:41 +0900

    1. priority queue

---
+++ data-structure/heap/priorityQueue.ts
@@ -0,0 +1,15 @@
+import { Heap } from "../heap";
+
+export class PriorityQueue<T> extends Heap<T> {
+  constructor(comparator: (a: T, b: T) => number) {
+    super(comparator);
+  }
+
+  enqueue(item: T): void {
+    this.insert(item);
+  }
+
+  dequeue(): T | null {
+    return this.remove();
+  }
+}
